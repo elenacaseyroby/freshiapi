07526a2182447017a04aec58a964c25cfe7c2142
Casey Roby <elenacaseyroby@gmail.com>
2021-01-26 00:13:32 -0500

    make admin forms for nutrient unit and unit conversion

--- django_apps/foods/admin.py
+++ django_apps/foods/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Nutrient, Unit, UnitConversion
+
+
+@admin.register(Nutrient)
+class NutrientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'dv_qty', 'dv_unit', 'usda_nutrient_id']
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ['name', 'abbr']
+
+
+@admin.register(UnitConversion)
+class UnitConversionAdmin(admin.ModelAdmin):
+    list_display = ['from_unit', 'to_unit', 'qty_conversion_coefficient']
