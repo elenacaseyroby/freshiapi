566c4adab2a99c6026947628a8456ab9d46ee2ba
Casey Roby <elenacaseyroby@gmail.com>
2021-02-08 14:50:48 -0500

    env vars should be set by postdeploy

--- .platform/hooks/postdeploy/01_run_migrations.sh
+++ .platform/hooks/postdeploy/01_run_migrations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Start virtual env.
 source /var/app/venv/*/bin/activate
-cd /var/app/staging
+cd /var/app/current
 # Run migrations
 # Need --noinput so it executes even if it meanst overwriting.
 python manage.py migrate --noinput
