7ce51abe731184d086a6f7a90ee429dc19bc225c
Casey Roby <elenacaseyroby@gmail.com>
2021-02-10 16:50:18 -0500

    create tests folder to test recipe scrape

---
+++ django_apps/recipes/tests/tests.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+# To run:
+# python manage.py test django_apps.recipes.tests.tests.
+# class ScrapeTitleTestCase(TestCase):
+
+# class ScrapeImageTestCase(TestCase):
+
+# class ScrapeSourceTestCase(TestCase):
+
+# class ScrapeServingsTestCase(TestCase):
+
+# class ScrapeTimeTestCase(TestCase):
+
+# class MatchIngredient(TestCase):
+
+# class MatchUnitTestCase(TestCase):
+
+# class MatchSourceTestCase(TestCase):
+
+# class ScrapeIngredientQty(TestCase):
+
+# class ScrapeIngredientUnit(TestCase):
+
+# class ScrapeIngredientsTestCase(TestCase):
+
+# class ScrapeIngredientsBySubgroupTestCase(TestCase):
+
+# class ScrapeDirectionsTestCase(TestCase):
+
+# class ScrapeDirectionsBySubgroupTestCase(TestCase):
+
+# class CalculateRecipeNutrients(TestCase):
+
+# class LoginRequiredTestCase(TestCase):
+
+# class DynamicWebsiteTestCase(TestCase):
+
+# class ScrapeDietsTestCase(TestCase):
+
+# class ScrapeCuisinesTestCase(TestCase):
+
+# class ScrapeAllergensTestCase(TestCase):
