245583e6f783ce7104b6c365599d015eb6201b33
Casey Roby <elenacaseyroby@gmail.com>
2021-02-12 22:39:07 -0500

    author

---
+++ django_apps/recipes/services/recipe_author_scraper.py
@@ -0,0 +1,32 @@
+def scrape_author_meta(soup_html):
+    # <meta name="author" content="Canal House">
+    author = soup_html.find(
+        'meta',
+        {'name': 'author'}
+    )
+    if author:
+        author = author['content']
+    return author
+
+
+def scrape_epicurious_author(soup_html):
+    # <a class="contributor"
+    # href="/contributors/sohui-kim"
+    # itemprop="author"
+    # rel="author"
+    # title="Sohui Kim"
+    # data-reactid="2">Sohui Kim</a>
+    author = soup_html.find(
+        'a',
+        {'class': 'contributor'}
+    )
+    if author:
+        author = author.get_text()
+    return author
+
+
+def scrape_recipe_author(soup_html):
+    author = scrape_author_meta(soup_html)
+    if not author:
+        author = scrape_epicurious_author(soup_html)
+    return author
--- django_apps/recipes/tests/tests.py
+++ django_apps/recipes/tests/tests.py
@@ -19,22 +19,31 @@ from django_apps.recipes.services.recipe_servings_scraper import (
     scrape_recipe_servings_count,
 )
 
+from django_apps.recipes.services.recipe_author_scraper import (
+    scrape_recipe_author,
+)
+
+
+def get_soup_html(url):
+    page = requests.get(url)
+    soup_html = BeautifulSoup(page.content, 'html.parser')
+    return soup_html
+
 
 # To run:
 # python manage.py test django_apps.recipes.tests.tests.ScrapeTitleTestCase
 class ScrapeTitleTestCase(TestCase):
     def test_find_bon_appetit_title(self):
-        url = 'https://www.bonappetit.com/recipe/seared-short-ribs-with-mushrooms'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.bonappetit.com/recipe/seared-short-ribs-with-mushrooms')
         title = scrape_recipe_title(soup_html)
         self.assertEqual(
             title, 'Seared Short Ribs With Mushrooms Recipe | Bon Appétit')
 
     def test_find_min_baker_title(self):
-        url = 'https://minimalistbaker.com/easy-baked-cheesecake-vegan-gf/'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://minimalistbaker.com/easy-baked-cheesecake-vegan-gf/'
+        )
         title = scrape_recipe_title(soup_html)
         self.assertEqual(
             title,
@@ -52,9 +61,9 @@ class ScrapeTitleTestCase(TestCase):
         )
 
     def test_find_leites_title(self):
-        url = 'https://leitesculinaria.com/80237/recipes-burrata-with-asparagus.html'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://leitesculinaria.com/80237/recipes-burrata-with-asparagus.html'
+        )
         title = scrape_recipe_title(soup_html)
         self.assertEqual(
             title,
@@ -62,9 +71,9 @@ class ScrapeTitleTestCase(TestCase):
         )
 
     def test_find_food_title(self):
-        url = 'https://www.food.com/recipe/chili-lime-cumin-cod-131528'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.food.com/recipe/chili-lime-cumin-cod-131528'
+        )
         title = scrape_recipe_title(soup_html)
         self.assertEqual(
             title,
@@ -72,9 +81,9 @@ class ScrapeTitleTestCase(TestCase):
         )
 
     def test_find_epicurious_title(self):
-        url = 'https://www.epicurious.com/recipes-menus/sohui-kim-lunar-new-year-menu-korean-article'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.epicurious.com/recipes-menus/sohui-kim-lunar-new-year-menu-korean-article'
+        )
         title = scrape_recipe_title(soup_html)
         self.assertEqual(
             title,
@@ -82,9 +91,9 @@ class ScrapeTitleTestCase(TestCase):
         )
 
     def test_find_smitten_kitchen_title(self):
-        url = 'https://smittenkitchen.com/2020/05/simple-essential-bolognese/'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://smittenkitchen.com/2020/05/simple-essential-bolognese/'
+        )
         title = scrape_recipe_title(soup_html)
         self.assertEqual(
             title,
@@ -94,49 +103,49 @@ class ScrapeTitleTestCase(TestCase):
 
 class ScrapeImageTestCase(TestCase):
     def test_find_bon_appetit_image(self):
-        url = 'https://www.bonappetit.com/recipe/seared-short-ribs-with-mushrooms'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.bonappetit.com/recipe/seared-short-ribs-with-mushrooms'
+        )
         image_url = scrape_recipe_image_url(soup_html)
         self.assertEqual(
             image_url, 'https://assets.bonappetit.com/photos/5ff8aaf6a2c5a800b6a67ef0/16:9/w_1280,c_limit/Family-Meal-Short-Ribs.jpg')
 
     def test_find_min_baker_image(self):
-        url = 'https://minimalistbaker.com/easy-baked-cheesecake-vegan-gf/'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://minimalistbaker.com/easy-baked-cheesecake-vegan-gf/'
+        )
         image_url = scrape_recipe_image_url(soup_html)
         self.assertEqual(
             image_url, 'https://minimalistbaker.com/wp-content/uploads/2016/03/BAKED-Vegan-Gluten-Free-Cheesecake-made-in-the-BLENDER-vegan-glutenfree-cheesecake-recipe.jpg')
 
     def test_find_dinner_at_the_zoo_image(self):
-        url = 'https://www.dinneratthezoo.com/sesame-noodles/'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.dinneratthezoo.com/sesame-noodles/'
+        )
         image_url = scrape_recipe_image_url(soup_html)
         self.assertEqual(
             image_url, 'https://www.dinneratthezoo.com/wp-content/uploads/2020/08/sesame-noodles-4.jpg')
 
     def test_find_leites_image(self):
-        url = 'https://leitesculinaria.com/80237/recipes-burrata-with-asparagus.html'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://leitesculinaria.com/80237/recipes-burrata-with-asparagus.html'
+        )
         image_url = scrape_recipe_image_url(soup_html)
         self.assertEqual(
             image_url, 'https://s23991.pcdn.co/wp-content/uploads/2019/04/burrata-asparagus-pine-nuts-raisins-fp.jpg.optimal.jpg')
 
     def test_find_epicurious_image(self):
-        url = 'https://www.epicurious.com/recipes-menus/sohui-kim-lunar-new-year-menu-korean-article'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.epicurious.com/recipes-menus/sohui-kim-lunar-new-year-menu-korean-article'
+        )
         image_url = scrape_recipe_image_url(soup_html)
         self.assertEqual(
             image_url, 'https://assets.epicurious.com/photos/6010782823e2f349f099cd0b/16:9/w_1280,c_limit/PorkTofuDumpling_HERO_012521_328_VOG_final.jpg')
 
     def test_find_smitten_kitchen_image(self):
-        url = 'https://smittenkitchen.com/2020/05/simple-essential-bolognese/'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://smittenkitchen.com/2020/05/simple-essential-bolognese/'
+        )
         image_url = scrape_recipe_image_url(soup_html)
         self.assertEqual(
             image_url,
@@ -144,9 +153,9 @@ class ScrapeImageTestCase(TestCase):
         )
 
     def test_find_budget_bytes_image(self):
-        url = 'https://www.budgetbytes.com/mediterranean-tuna-salad/'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.budgetbytes.com/mediterranean-tuna-salad/'
+        )
         image_url = scrape_recipe_image_url(soup_html)
         self.assertEqual(
             image_url,
@@ -157,9 +166,9 @@ class ScrapeImageTestCase(TestCase):
 class ScrapeSourceTestCase(TestCase):
 
     def test_find_bon_appetit_source_name(self):
-        url = 'https://www.bonappetit.com/recipe/seared-short-ribs-with-mushrooms'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.bonappetit.com/recipe/seared-short-ribs-with-mushrooms'
+        )
         source_name = scrape_recipe_source_name(soup_html)
         self.assertEqual(
             source_name,
@@ -178,9 +187,9 @@ class ScrapeSourceTestCase(TestCase):
 class ScrapeServingsTestCase(TestCase):
 
     def test_find_bon_appetit_servings_count(self):
-        url = 'https://www.bonappetit.com/recipe/seared-short-ribs-with-mushrooms'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.bonappetit.com/recipe/seared-short-ribs-with-mushrooms'
+        )
         servings_count = scrape_recipe_servings_count(soup_html)
         self.assertEqual(
             servings_count,
@@ -188,9 +197,9 @@ class ScrapeServingsTestCase(TestCase):
         )
 
     def test_find_wordpress_recipe_maker_servings_count(self):
-        url = 'https://minimalistbaker.com/easy-baked-cheesecake-vegan-gf/'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://minimalistbaker.com/easy-baked-cheesecake-vegan-gf/'
+        )
         servings_count = scrape_recipe_servings_count(soup_html)
         self.assertEqual(
             servings_count,
@@ -198,9 +207,9 @@ class ScrapeServingsTestCase(TestCase):
         )
 
     def test_find_dinner_at_zoo_servings_count(self):
-        url = 'https://www.dinneratthezoo.com/sesame-noodles/'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.dinneratthezoo.com/sesame-noodles/'
+        )
         servings_count = scrape_recipe_servings_count(soup_html)
         self.assertEqual(
             servings_count,
@@ -208,9 +217,9 @@ class ScrapeServingsTestCase(TestCase):
         )
 
     def test_find_leites_servings_count(self):
-        url = 'https://leitesculinaria.com/80237/recipes-burrata-with-asparagus.html'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://leitesculinaria.com/80237/recipes-burrata-with-asparagus.html'
+        )
         servings_count = scrape_recipe_servings_count(soup_html)
         self.assertEqual(
             servings_count,
@@ -218,13 +227,36 @@ class ScrapeServingsTestCase(TestCase):
         )
 
     def test_website_with_different_yield_count(self):
-        url = 'https://www.foodandwine.com/recipes/old-fashioned-banana-bread'
-        page = requests.get(url)
-        soup_html = BeautifulSoup(page.content, 'html.parser')
+        soup_html = get_soup_html(
+            'https://www.foodandwine.com/recipes/old-fashioned-banana-bread'
+        )
         servings_count = scrape_recipe_servings_count(soup_html)
         self.assertEqual(
             servings_count,
             None
+        )
+
+
+class ScrapeAuthorTestCase(TestCase):
+
+    def test_find_bon_appetit_author(self):
+        soup_html = get_soup_html(
+            'https://www.bonappetit.com/recipe/seared-short-ribs-with-mushrooms'
+        )
+        author = scrape_recipe_author(soup_html)
+        self.assertEqual(
+            author,
+            'Molly Baz'
+        )
+
+    def test_find_epicurious_author(self):
+        soup_html = get_soup_html(
+            'https://www.epicurious.com/recipes/food/views/pork-and-chive-dumplings-sohui-kim'
+        )
+        author = scrape_recipe_author(soup_html)
+        self.assertEqual(
+            author,
+            'Sohui Kim'
         )
 
     # class ScrapeServingsTestCase(TestCase):
