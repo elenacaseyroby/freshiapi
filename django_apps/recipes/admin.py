a55c0a4094e3db3eb6d6e4cc43f0286ba952c342
Casey Roby <elenacaseyroby@gmail.com>
2021-02-17 20:47:13 -0500

    admin page works

--- django_apps/recipes/admin.py
+++ django_apps/recipes/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+# from django import forms
 
-# Register your models here.
+from .models import Recipe
+from .scraper.recipe_scraper import scrape_recipe
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    fields = ('url',)
+    readonly_fields = (
+        'title',
+        'author',
+        'description',
+        'prep_time',
+        'cook_time',
+        'total_time',
+    )
+    exclude = ['nutrition_facts_complete', ]
+
+    # generate_nutrition_facts = forms.Boolean(widget=forms.CheckboxInput())
+
+    def save_model(self, request, obj, form, change):
+        # obj.user = request.user
+        scrape_recipe(obj.url)
+        # super().save_model(request, obj, form, change)
