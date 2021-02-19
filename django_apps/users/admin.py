7055d7b4b9be2431930e08d58be028e7ee4a16f8
Casey Roby <elenacaseyroby@gmail.com>
2021-02-18 19:56:33 -0500

    user admin

--- django_apps/users/admin.py
+++ django_apps/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ('first_name', 'last_name', 'email', 'username')
