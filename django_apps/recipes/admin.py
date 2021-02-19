b13f46c95b2523ee56b6cc97e5e80e580f988a32
Casey Roby <elenacaseyroby@gmail.com>
2021-02-18 21:52:51 -0500

    add recipe admin page with nutrition facts

--- django_apps/recipes/admin.py
+++ django_apps/recipes/admin.py
@@ -1,26 +1,76 @@
 from django.contrib import admin
 # from django import forms
 
-from .models import Recipe, Direction
+from .models import (
+    Recipe, Direction, Ingredient, NutritionFact)
 from .scraper.recipe_scraper import scrape_recipe
 
 
 class DirectionInline(admin.TabularInline):
     model = Direction
+    extra = 1
     fields = (
         'step',
         'text'
     )
 
 
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 1
+    fields = (
+        'food',
+        'qty_numerator',
+        'qty_denominator',
+        'qty_unit',
+    )
+
+
+class NutritionFactInline(admin.TabularInline):
+    model = NutritionFact
+    extra = 0
+
+    # custom fields must be in readonly_fields
+    def dv_percent(self, obj):
+        if not obj.nutrient.dv_qty:
+            return '--'
+        recipe_nutrient_qty = float(obj.nutrient_qty)
+        nutrient_dv_qty = float(obj.nutrient.dv_qty)
+        percent = str(round(recipe_nutrient_qty / nutrient_dv_qty) * 100)
+        return f'{percent}%'
+
+    # custom fields must be in readonly_fields
+    def nutrient_unit(self, obj):
+        if not obj.nutrient.dv_unit:
+            return '--'
+        return obj.nutrient.dv_unit.abbr
+
+    fields = (
+        'nutrient',
+        'nutrient_qty',
+        'nutrient_unit',
+        'dv_percent'
+    )
+    readonly_fields = (
+        'nutrient',
+        'nutrient_qty',
+        'nutrient_unit',
+        'dv_percent'
+    )
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
 
-    inlines = [DirectionInline, ]
+    inlines = [
+        DirectionInline,
+        NutritionFactInline
+    ]
 
     fields = (
         'url',
         'title',
+        'servings_count',
         'author',
         'description',
         'prep_time',
@@ -30,6 +80,15 @@ class RecipeAdmin(admin.ModelAdmin):
         'nutrition_facts_complete',
     )
     readonly_fields = (
+        'title',
+        'servings_count',
+        'author',
+        'description',
+        'prep_time',
+        'cook_time',
+        'total_time',
+        'source',
+        'nutrition_facts_complete',
         'nutrition_facts_complete',
     )
     # ingredients
@@ -39,7 +98,8 @@ class RecipeAdmin(admin.ModelAdmin):
     # diets
 
     def save_model(self, request, obj, form, change):
-        super().save_model(request, obj, form, change)
+        # this will double save so must be commented:
+        # super().save_model(request, obj, form, change)
         scrape_recipe(obj.url)
 
 
