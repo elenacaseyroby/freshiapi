8ff93afc44284eb087a88d63771d05b1c322f373
Casey Roby <elenacaseyroby@gmail.com>
2021-02-23 15:35:38 -0500

    try to add commands to source .env

--- .platform/hooks/postdeploy/01_set_env.sh
+++ .platform/hooks/postdeploy/01_set_env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Create a copy of the environment variable file.
-sudo cp /opt/elasticbeanstalk/deployment/env /var/app/current/backend/.env
 
-# Set permissions to the .env file so this file can be accessed 
+# Create a copy of the environment variable file.
+cp /opt/elasticbeanstalk/deployment/env /opt/elasticbeanstalk/deployment/custom_env_var
+
+# Set permissions to the custom_env_var file so this file can be accessed 
 # by any user on the instance. You can restrict permissions as per your requirements.
-chmod 755 /var/app/current/backend/.env
+chmod 644 /opt/elasticbeanstalk/deployment/custom_env_var
+
+# Remove duplicate files upon deployment.
+rm -f /opt/elasticbeanstalk/deployment/*.bak
