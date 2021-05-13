a222f4a240f2259eb56027bdacf5dfda63cbace3
Casey Roby <elenacaseyroby@gmail.com>
2021-05-13 01:29:29 -0400

    fix so django admin login works again

--- backend/settings.py
+++ backend/settings.py
@@ -102,6 +102,10 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
