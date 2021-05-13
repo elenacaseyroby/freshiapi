f865a422f220243098d485fdff6114b6b3896033
Casey Roby <elenacaseyroby@gmail.com>
2021-05-13 12:58:41 -0400

    use authentication instead of permission

--- django_apps/users/api_views.py
+++ django_apps/users/api_views.py
@@ -4,11 +4,10 @@ from django_apps.users.serializers import UserSerializer
 from django_apps.users.models import User
 
 from django_apps.api_auth.authentication import APIAuthentication
-from django_apps.api_auth.permissions import APIIsAuthenticated
 
 
 class UserList(ListAPIView):
+    # Must prove logged in by passing Authentication token in header.
     authentication_classes = (APIAuthentication, )
-    # permission_classes = (APIIsAuthenticated,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
