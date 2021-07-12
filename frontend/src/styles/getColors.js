5ac57bf5af40e1230c51bd679a0c75390f07e0c8
Casey Roby <elenacaseyroby@gmail.com>
2021-07-12 15:53:07 -0400

    include opacity in colors

--- frontend/src/styles/getColors.js
+++ frontend/src/styles/getColors.js
@@ -2,16 +2,46 @@ const getColors = (lightMode) => {
   // lightMode: Bool
   // return object
   const colors = {
-    background: lightMode ? '#ffffff' : '#242e36',
-    highContrast: lightMode ? '#242e36' : '#ffffff',
-    midContrast: lightMode ? '#667079' : '#e7ecf0',
-    lowContrast: lightMode ? '#f5f8fa' : '#667079',
-    highlight: lightMode ? '#e7ecf0' : '#f5f8fa',
-    interactiveFocus: lightMode ? '#cc3a16' : '#3d96ff',
-    error: '#ff3b30',
-    success: '#34c759',
-    info: '#0a7aff',
-    shadow: lightMode ? '#9b9a9a' : '#000000', // opacity 50%
+    background: {
+      hex: lightMode ? '#ffffff' : '#242e36',
+      opacity: '100%',
+    },
+    highContrast: {
+      hex: lightMode ? '#242e36' : '#ffffff',
+      opacity: '100%',
+    },
+    midContrast: {
+      hex: lightMode ? '#667079' : '#e7ecf0',
+      opacity: '100%',
+    },
+    lowContrast: {
+      hex: lightMode ? '#f5f8fa' : '#667079',
+      opacity: '100%',
+    },
+    highlight: {
+      hex: lightMode ? '#e7ecf0' : '#f5f8fa',
+      opacity: '100%',
+    },
+    interactiveFocus: {
+      hex: lightMode ? '#cc3a16' : '#3d96ff',
+      opacity: '100%',
+    },
+    error: {
+      hex: '#ff3b30',
+      opacity: '100%',
+    },
+    success: {
+      hex: '#34c759',
+      opacity: '100%',
+    },
+    info: {
+      hex: '#0a7aff',
+      opacity: '100%',
+    },
+    shadow: {
+      hex: lightMode ? '#9b9a9a' : '#000000',
+      opacity: '50%',
+    }
   };
   return colors;
 };
