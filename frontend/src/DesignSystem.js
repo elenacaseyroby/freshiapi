abde998ccfdd3864ae23cb6795976ed8ba3cac92
Casey Roby <elenacaseyroby@gmail.com>
2021-07-12 13:06:02 -0400

    programatic approach

--- frontend/src/DesignSystem.js
+++ frontend/src/DesignSystem.js
@@ -7,17 +7,13 @@ const DesignSystem = ({ media }) => {
   const fonts = getFonts(media.windowWidth);
   return (
     <div>
-      <div style={fonts.largeTitle}>largeTitle</div>
-      <div style={fonts.title1}>title1</div>
-      <div style={fonts.title2}>title2</div>
-      <div style={fonts.title3}>title3</div>
-      <div style={fonts.headline}>body</div>
-      <div style={fonts.body}>body</div>
-      <div style={fonts.callout}>callout</div>
-      <div style={fonts.subhead}>subhead</div>
-      <div style={fonts.footnote}>footnote</div>
-      <div style={fonts.caption1}>caption1</div>
-      <div style={fonts.caption2}>caption2</div>
+      {/* fonts */}
+      <div>
+        { Object.keys(fonts).map((key) => {
+          const style = fonts[key];
+          return (<div style={style}>{key}</div>);
+        })}
+      </div>
     </div>
   );
 };
