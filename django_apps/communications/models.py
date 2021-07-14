0ff8a9aee63798a5ae128654fdd988bca0cc80ea
Casey Roby <elenacaseyroby@gmail.com>
2021-07-14 16:19:24 -0400

    update email model

--- django_apps/communications/models.py
+++ django_apps/communications/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.utils.functional import cached_property
 
 
 class Email(models.Model):
@@ -11,19 +13,27 @@ class Email(models.Model):
         blank=False,
         null=False
     )
-    to_email = models.EmailField(
-        verbose_name='To email',
-        blank=False,
-        null=False
+    to_emails = ArrayField(
+        models.EmailField(
+            verbose_name='To email',
+            blank=True
+        ),
     )
     subject = models.CharField(max_length=255, null=True, blank=True)
-    body = models.TextField(null=False, blank=False)
+    msg = models.TextField(null=True, blank=True)
+    html_msg = models.TextField(null=True, blank=True)
+    # time attempted to send
+    sent_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     error = models.TextField(null=True, blank=True)
-    note = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    @cached_property
+    def sent_successfully(self):
+        if self.error:
+            return False
+        return True
 
     def __str__(self):
-        return self.email
+        return f"subject: '{self.subject}', to: {str(self.to_emails)}"
 
     class Meta:
         db_table = 'communications_emails'
